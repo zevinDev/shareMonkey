55583346adaaaec8103809100e840ee6bc30a351
Zevin Burzynski <86094612+REALziez@users.noreply.github.com>
2023-01-02 17:58:22 -0700

    Fixed dependencies and added notification tab

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, DrawerActions, useNavigation } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Image } from 'react-native';
 
 import { themeColor, useTheme } from "react-native-rapi-ui";
 import TabBarIcon from "../components/utils/TabBarIcon";
 import TabBarText from "../components/utils/TabBarText";
+import { Icon } from 'react-native-elements'
 
 
 import Home from "../screens/Home";
@@ -14,28 +16,50 @@ import Post from "../screens/Post"
 import SecondScreen from "../screens/SecondScreen";
 import Search from "../screens/Search"
 import Profile from "../screens/Profile";
+import Leaderboard from "../screens/Leaderboard";
 
 const Drawer = createDrawerNavigator();
 
 const MainStack = createNativeStackNavigator();
+
 const Main = () => {
+  const { isDarkmode } = useTheme();
+  const navigation = useNavigation();
   return (
     <MainStack.Navigator
       screenOptions={{
-        headerShown: false,
+        headerShown: true, headerStyle: {backgroundColor: isDarkmode ? themeColor.dark200 : "#ffffff"}
       }}
     >
-      <MainStack.Screen name="Tabs" component={MainTabs} />
+      <MainStack.Screen name="MyDrawer" component={MyDrawer} options={{
+            headerTitle: (props) => <LogoTitle {...props} />,
+            headerRight: () => (
+              <Icon 
+              onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())} 
+              name='notifications-outline'
+              type='ionicon'
+              color={isDarkmode ? "#ffffff" : "#000000"}
+              />
+            ),
+          }} />
     </MainStack.Navigator>
   );
 };
 
+function LogoTitle() {
+  return (
+    <Image
+      style={{ width: 50, height: 50 }}
+      source={require('../../assets/favicon.png')}
+    />
+  );
+}
+
 const MyDrawer = () => {
   const { isDarkmode } = useTheme();
   return (
-      <Drawer.Navigator initialRouteName="Home" screenOptions={{headerShown: false,drawerStyle: {borderTopColor: isDarkmode ? themeColor.dark100 : "#c0c0c0",backgroundColor: isDarkmode ? themeColor.dark200 : "#ffffff"},drawerLabelStyle:{color: isDarkmode ? "#ffffff" : "#000000"}}}>
-        <Drawer.Screen name="Home" component={Home} />
-        <Drawer.Screen name="SecondScreen" component={SecondScreen} options={{ headerShown: false }} />
+      <Drawer.Navigator initialRouteName="Home" screenOptions={{headerShown: false, drawerPosition:"right", drawerStyle: {borderTopColor: isDarkmode ? themeColor.dark100 : "#c0c0c0",backgroundColor: isDarkmode ? themeColor.dark200 : "#ffffff"},drawerLabelStyle:{color: isDarkmode ? "#ffffff" : "#000000"}}}>
+        <Drawer.Screen name="Notification" component={MainTabs} />
       </Drawer.Navigator>
   );
 }
@@ -56,7 +80,7 @@ const MainTabs = () => {
       {/* these icons using Ionicons */}
       <Tabs.Screen
         name="Home"
-        component={MyDrawer}
+        component={Home}
         options={{
           tabBarLabel: ({ focused }) => (
             <TabBarText focused={focused} title="Home" />
@@ -87,6 +111,18 @@ const MainTabs = () => {
           ),
           tabBarIcon: ({ focused }) => (
             <TabBarIcon focused={focused} icon={"add-circle-outline"} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Leaderboard"
+        component={Leaderboard}
+        options={{
+          tabBarLabel: ({ focused }) => (
+            <TabBarText focused={focused} title="Leaderboard" />
+          ),
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} icon={"podium-outline"} />
           ),
         }}
       />
