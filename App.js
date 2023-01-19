afd4658a44e799e2e2ca2206fa5809e6d2c99506
Zevin Burzynski <86094612+REALziez@users.noreply.github.com>
2023-01-18 18:14:33 -0700

    New Logo For Branch

--- App.js
+++ App.js
@@ -5,11 +5,14 @@ import { ThemeProvider } from "react-native-rapi-ui";
 import * as Sentry from 'sentry-expo';
 
 Sentry.init({
-  dsn: 'https://6a6de79a5b574e629aac6d84769bcd23@o4504528218947584.ingest.sentry.io/4504528219996160',
+  dsn: 'https://d831b6c92dfc40aab8adaa66e05f10be@o4504528218947584.ingest.sentry.io/4504528497016832',
   enableInExpoDevelopment: true,
   debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+
 });
 
+
+Sentry.Native.captureException(Error("My first Sentry error!"));
 
 export default function App() {
   return (
