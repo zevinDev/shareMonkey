2a8aff200e97fee9192d5a8063ad70347e187707
arcbarkzap <71309451+arcbarkzap@users.noreply.github.com>
2023-04-03 15:11:29 -0700

    feat: documentation
    Co-Authored-By: Zevin Burzynski <86094612+REALziez@users.noreply.github.com>

--- src/screens/Documentation.js
+++ src/screens/Documentation.js
@@ -9,7 +9,10 @@ export default function ({ navigation }) {
         contentInsetAdjustmentBehavior="automatic"
         style={{ height: "100%" }}
       >
-        <Text> Documentation </Text>
+        <Text size="h1" fontWeight="bold" style={{justifyContent: "center"}}> Documentation </Text>
+        <Text size="h2"> What Is ShareMonkey? </Text>
+        <Text size="lg">ShareMonkey is a mobile application designed for use in school districts and other similar enviroments. It is used to help share information between faculty members and students, as well as build school spirit. This is achieved through the ability of users to post images of themselves in attendance of school events and earn points for the in-app leaderboard. Additional information regarding the application and its features can be found in the INFORMATION file.</Text>
+
       </ScrollView>
     </SafeAreaView>
   );
