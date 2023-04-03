8a22c9a6234541667426a56e6cb7e2ba1d9cc566
arcbarkzap <71309451+arcbarkzap@users.noreply.github.com>
2023-04-03 16:08:56 -0700

    fix: in app browser

--- src/screens/Documentation.js
+++ src/screens/Documentation.js
@@ -1,11 +1,8 @@
 import { React } from "react";
-import { WebView } from "react-native";
+import { WebView } from 'react-native-webview';
 
 export default function ({ navigation }) {
   return (
-    <WebView
-    source={{uri: 'https://github.com/REALziez/Frontend/#readme'}}
-    style={{marginTop: 20}}
-  />
+  <WebView source={{ uri: 'https://github.com/REALziez/Frontend/#readme' }} style={{ flex: 1 }} />
   );
 }
